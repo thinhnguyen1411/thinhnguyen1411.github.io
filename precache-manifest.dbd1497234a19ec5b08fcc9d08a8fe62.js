9cd6be6b1a944979c9ed607ecb9205407a0b98d6
thinhnguyen1411 <thinhnguyen1411@users.noreply.github.com>
2019-03-23 15:31:47 +0700

    Add files via upload

---
+++ precache-manifest.dbd1497234a19ec5b08fcc9d08a8fe62.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "04b2c35d2dd74bc33957",
+    "url": "/static/js/main.04b2c35d.chunk.js"
+  },
+  {
+    "revision": "022cb2a1d0b7c1973b45",
+    "url": "/static/js/2.022cb2a1.chunk.js"
+  },
+  {
+    "revision": "04b2c35d2dd74bc33957",
+    "url": "/static/css/main.0341375a.chunk.css"
+  },
+  {
+    "revision": "f334f3fb6d67570be11a7b52c42e6d3f",
+    "url": "/index.html"
+  }
+];
